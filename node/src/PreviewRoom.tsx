6f60dc755c004d4646352a96e951e5a1a04d2c93
Jan <33833587+Model2TypeE@users.noreply.github.com>
2020-07-13 15:13:15 +0200

    Preview Room with input options and react-like rendering improvements

--- node/src/PreviewRoom.tsx
+++ node/src/PreviewRoom.tsx
@@ -1,58 +1,75 @@
 import React from 'react';
 import * as SWRTC from '@andyet/simplewebrtc';
 import MediaPreview from './components/MediaPreview';
+import { UserMediaIds } from '@andyet/simplewebrtc/components/RequestUserMedia';
 
 interface Props {
     store: any;
 };
 interface State {
     permissions: boolean;
-    videoMedia?: SWRTC.Media;
+    videoEnabled: boolean;
+    audioEnabled: boolean;
 };
 export default class PreviewRoom extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
 
+        this.permissionsGranted = this.permissionsGranted.bind(this);
+        this.permissionsDenied = this.permissionsDenied.bind(this);
+        this.toggleAudio = this.toggleAudio.bind(this);
+        this.toggleVideo = this.toggleVideo.bind(this);
+
         this.state = {
-            permissions: false
+            permissions: false,
+            videoEnabled: true,
+            audioEnabled: true
         };
-
-        const constraints = {
-            video: true,
-            audio: true
-        }
-
-        // navigator.mediaDevices.getUserMedia(constraints)
-        //     .then((stream) => {
-        //         this.setState((state) => {
-        //             return {
-        //                 permissions: true,
-        //                 videoMedia: {
-        //                     createdAt: 0, id: 'test', kind: "video", remoteDisabled: false, localDisabled: false, screenCapture: false, speaking: false,
-        //                     renderMirrored: false, volume: 1, source: "local", stream: stream, track: stream.getVideoTracks()[0]
-        //                 }
-        //             };
-        //         })
-        //     })
-        //     .catch((e) => {
-        //         console.log(e);
-        //     })
     }
 
+    permissionsGranted(trackIds?: UserMediaIds) {
+        this.setState((state) => ({
+            permissions: true,
+            videoEnabled: state.videoEnabled,
+            audioEnabled: state.audioEnabled
+        }));
+    }
+
+    toggleAudio() {
+        this.setState((state) => ({
+            permissions: state.permissions,
+            videoEnabled: state.videoEnabled,
+            audioEnabled: !state.audioEnabled
+        }));
+    }
+
+    toggleVideo() {
+        this.setState((state) => ({
+            permissions: state.permissions,
+            videoEnabled: !state.videoEnabled,
+            audioEnabled: state.audioEnabled
+        }));
+    }
+
+    permissionsDenied(err?: Error) { console.log(err); }
+
     render() {
-        console.log('videoView rendered');
-        console.log(this.state.videoMedia);
+        console.log('PreviewRoom rendered');
         return (
             <div>
-                <SWRTC.RequestUserMedia audio video auto />
+                {/* Get permissions to use the user's media devices */}
+                permissions ? : (
+                <SWRTC.RequestUserMedia audio video auto
+                    onSuccess={this.permissionsGranted}
+                    onError={this.permissionsDenied} />)
+
+                {/* Preview the Media that was returned during the media request */}
                 <SWRTC.LocalMediaList
                     screen={false}
                     render={({ media }) => {
-                        console.log(media);
-
-                        // const previewVideo = media.filter(m => m.id === this.state.previewVideoId)[0];
-                        // const previewAudio = media.filter(m => m.id === this.state.previewAudioId)[0];
-                        return <MediaPreview video={media[1]} toggleVideo={null} toggleAudio={null} />
+                        const previewVideo = this.state.videoEnabled ? media.filter(m => m.kind === "video")[0] : undefined;
+                        const previewAudio = this.state.audioEnabled ? media.filter(m => m.kind === "audio")[0] : undefined;
+                        return <MediaPreview video={previewVideo} toggleVideo={this.toggleVideo} toggleAudio={this.toggleAudio} />
                     }}
                 />
             </div>
--- node/src/components/mediaPreview.tsx
+++ node/src/components/mediaPreview.tsx
@@ -1,9 +1,11 @@
-import { Media, Video } from '@andyet/simplewebrtc';
+import { Media, Video, Audio } from '@andyet/simplewebrtc';
 import React from 'react';
 import styled from 'styled-components';
+import { ProgressPlugin } from 'webpack';
 
 const VideoContainer = styled.div({
     width: '100%',
+    height: '450px',
     position: 'relative',
     padding: '0',
     margin: '0',
@@ -13,13 +15,15 @@ const VideoContainer = styled.div({
 
     '& video': {
         width: '100%',
+        height: '100%',
+        background: '#000000',
         objectFit: 'contain',
     },
 });
 
 const BlankVideo = styled.div({
     width: '100%',
-    height: '300px',
+    height: '450px',
     backgroundColor: '#262a2c',
     color: '#e9ecec',
     display: 'flex',
@@ -43,33 +47,89 @@ const SubContainer = styled.div({
     backgroundColor: '#262a2c',
 });
 
-interface MediaPreviewProps {
+interface Props {
     video?: Media;
+    audio?: Media;
     toggleVideo: any;
     toggleAudio: any;
 }
-
-function onclick() {
-    // config.videoSettings.data1 = 'ASDASD';
+interface State {
+    usernameInput: string;
 }
+class MediaPreview extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
 
-const MediaPreview: React.SFC<MediaPreviewProps> = ({ video, toggleVideo, toggleAudio }) => (
-    <FullContainer>
-        <SubContainer>
-            <VideoContainer>
-                {
-                    video && video.loaded
-                        ? <Video media={video} />
-                        : <BlankVideo> <p>No video selected</p> </BlankVideo>
-                }
-            </VideoContainer>
-        </SubContainer>
-        <SubContainer>
-            <button onClick={toggleVideo}>
-                CLICK ME
-            </button>
-        </SubContainer>
-    </FullContainer>
-);
+        this.handleUsernameInput = this.handleUsernameInput.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
+        this.state = {
+            usernameInput: ''
+        };
+    }
+
+    handleUsernameInput(event: React.ChangeEvent<HTMLInputElement>) {
+        event.persist();
+        this.setState((state) => ({
+            usernameInput: event.target.value
+        }));
+    }
+
+    // enter room with given input data
+    handleClick() {
+        console.log(this.state.usernameInput);
+    }
+
+    render() {
+        return (
+            <FullContainer>
+                <SubContainer>
+                    <VideoContainer>
+                        {
+                            this.props.video && this.props.video.loaded
+                                ? <Video media={this.props.video} />
+                                : <BlankVideo> <p>No video selected</p> </BlankVideo>
+                        }
+                    </VideoContainer>
+                </SubContainer>
+                <SubContainer>
+                    <div>
+                        <button onClick={this.props.toggleVideo}> Toggle Video </button>
+                        <button onClick={this.props.toggleAudio}> ToggleAudio </button>
+                        <input type="text" placeholder={"Username"} autoFocus={true} onChange={this.handleUsernameInput} />
+                        <input
+                            type="button"
+                            value="Enter Room"
+                            onClick={this.handleClick}
+                        />
+                    </div>
+                </SubContainer>
+            </FullContainer>
+        );
+    }
+}
+// const MediaPreview: React.SFC<MediaPreviewProps> = ({ video, toggleVideo, toggleAudio }) => (
+//     <FullContainer>
+//         <SubContainer>
+//             <VideoContainer>
+//                 {
+//                     video && video.loaded
+//                         ? <Video media={video} />
+//                         : <BlankVideo> <p>No video selected</p> </BlankVideo>
+//                 }
+//             </VideoContainer>
+//         </SubContainer>
+//         <SubContainer>
+//             <button onClick={toggleVideo}> Toggle Video </button>
+//             <button onClick={toggleAudio}> ToggleAudio </button>
+//             <input type="text" onChange={this.handleChange} />
+//             <input
+//                 type="button"
+//                 value="Alert the text input"
+//                 onClick={this.handleClick}
+//             />
+//         </SubContainer>
+//     </FullContainer>
+// );
 
 export default MediaPreview;
